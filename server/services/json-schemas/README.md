32ef84e187e08bc69c71556e3703e3695f8dd14f
Daniele Sangalli <196739+daniel78uk@users.noreply.github.com>
2023-12-01 13:35:37 +0000

    feat: adding json schema validator service

---
+++ server/services/json-schemas/README.md
@@ -0,0 +1,3 @@
+# Validation schemas
+
+In this folder there are schemas used to do a JSON validation before they are sent to Azure service bus.
---
+++ tests/unit/services/schema-validator.service.spec.js
@@ -0,0 +1,52 @@
+const {
+  validateDataAgainstSchema
+} = require('../../../server/services/schema-validator.service.js')
+
+const wqJsonSchema = require('../../../server/services/json-schemas/sirp_json_schema_WQ_v0.3.json')
+const wqSampleResponse = require('../../samples/sample_water_quality_incident.json')
+
+describe('validateDataAgainstSchema function', () => {
+  it('should validate correctly against a valid schema', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        age: { type: 'number' }
+      },
+      required: ['name', 'age']
+    }
+
+    const data = {
+      name: 'John',
+      age: 30
+    }
+
+    const results = validateDataAgainstSchema(data, schema)
+
+    expect(results).toBe(true)
+  })
+
+  it('should return false and log errors for invalid data against schema', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        email: { type: 'string', format: 'email' }
+      },
+      required: ['email']
+    }
+
+    const data = {
+      email: 'notAnEmail'
+    }
+
+    const results = validateDataAgainstSchema(data, schema)
+
+    expect(results).toBe(false)
+  })
+
+  it('should compile and validate sample against a given schema', () => {
+    const results = validateDataAgainstSchema(wqSampleResponse,wqJsonSchema)
+
+    expect(results).toBe(true)
+  })
+})
