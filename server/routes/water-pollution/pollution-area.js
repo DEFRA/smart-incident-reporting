380f90cecb14e876dd8b2735f1a270eafcb974d9
Tedd Mason <teddmason@gmail.com>
2024-02-12 11:17:15 +0000

    sorting placeholders for sonarcloud

--- server/routes/water-pollution/pollution-area.js
+++ server/routes/water-pollution/pollution-area.js
@@ -1,10 +1,7 @@
 import constants from '../../utils/constants.js'
 
 const handlers = {
-  get: async (_request, h) => h.view(constants.views.WATER_POLLUTION_POLLUTION_AREA),
-  post: async (request, h) => {
-    return h.redirect(constants.routes.WATER_POLLUTION_POLLUTION_AREA)
-  }
+  get: async (_request, h) => h.view(constants.views.WATER_POLLUTION_POLLUTION_AREA)
 }
 
 export default [
@@ -12,10 +9,5 @@ export default [
     method: 'GET',
     path: constants.routes.WATER_POLLUTION_POLLUTION_AREA,
     handler: handlers.get
-  },
-  {
-    method: 'POST',
-    path: constants.routes.WATER_POLLUTION_POLLUTION_AREA,
-    handler: handlers.post
   }
 ]
--- server/routes/water-pollution/pollution-length.js
+++ server/routes/water-pollution/pollution-length.js
@@ -1,10 +1,7 @@
 import constants from '../../utils/constants.js'
 
 const handlers = {
-  get: async (_request, h) => h.view(constants.views.WATER_POLLUTION_POLLUTION_LENGTH),
-  post: async (request, h) => {
-    return h.redirect(constants.routes.WATER_POLLUTION_POLLUTION_LENGTH)
-  }
+  get: async (_request, h) => h.view(constants.views.WATER_POLLUTION_POLLUTION_LENGTH)
 }
 
 export default [
@@ -12,10 +9,5 @@ export default [
     method: 'GET',
     path: constants.routes.WATER_POLLUTION_POLLUTION_LENGTH,
     handler: handlers.get
-  },
-  {
-    method: 'POST',
-    path: constants.routes.WATER_POLLUTION_POLLUTION_LENGTH,
-    handler: handlers.post
   }
 ]
