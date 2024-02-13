51b710b23c57906fd72a3466858cc1b0101033f0
Tedd Mason <teddmason@gmail.com>
2024-02-13 10:50:17 +0000

    sonarcloud fix

--- server/routes/water-pollution/pollution-area.js
+++ server/routes/water-pollution/pollution-area.js
@@ -24,7 +24,7 @@ const handlers = {
     if (errorSummary.errorList.length > 0) {
       return h.view(constants.views.WATER_POLLUTION_POLLUTION_AREA, {
         errorSummary,
-        ...getContext(request)
+        ...getContext()
       })
     }
 
