61140f02c12e53291302fe6a5e3652dd668a7370
Tedd Mason <teddmason@gmail.com>
2024-02-22 15:01:16 +0000

    sonarcloud updates

--- server/routes/water-pollution/when.js
+++ server/routes/water-pollution/when.js
@@ -3,23 +3,7 @@ import constants from '../../utils/constants.js'
 
 const handlers = {
   get: async (_request, h) => h.view(constants.views.WATER_POLLUTION_WHEN),
-  post: async (request, h) => {
-    // const { locationDescription } = request.payload
-    // if (!locationDescription) {
-    //   const errorSummary = getErrorSummary()
-    //   errorSummary.errorList.push({
-    //     text: 'Enter a description of where the pollution is',
-    //     href: '#locationDescription'
-    //   })
-    //   return h.view(constants.views.WATER_POLLUTION_LOCATION_DESCRIPTION, {
-    //     errorSummary
-    //   })
-    // }
-
-    // request.yar.set(constants.redisKeys.WATER_POLLUTION_LOCATION_DESCRIPTION, locationDescription)
-
-    return h.redirect(constants.routes.WATER_POLLUTION_POLLUTION_APPEARANCE)
-  }
+  post: async (_request, h) => h.redirect(constants.routes.WATER_POLLUTION_POLLUTION_APPEARANCE)
 }
 
 export default [
