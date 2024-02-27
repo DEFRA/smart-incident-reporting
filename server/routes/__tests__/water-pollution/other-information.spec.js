455065003567a116ccde9cd36b6309a2b9aba6d9
Tedd Mason <teddmason@gmail.com>
2024-02-27 09:51:09 +0000

    SIR-633 make more information field optional and change header and title

--- server/routes/__tests__/water-pollution/other-information.spec.js
+++ server/routes/__tests__/water-pollution/other-information.spec.js
@@ -5,7 +5,7 @@ import { session } from '../../../__mock-data__/session-water-pollution.js'
 jest.mock('../../../services/service-bus.js')
 
 const url = constants.routes.WATER_POLLUTION_OTHER_INFORMATION
-const header = 'Can you describe the pollution?'
+const header = 'Is there anything else you\'d like to add (optional)?'
 
 describe(url, () => {
   describe('GET', () => {
@@ -61,16 +61,6 @@ describe(url, () => {
       }))
       expect(response.request.yar.get(constants.redisKeys.WATER_POLLUTION_OTHER_INFORMATION)).toEqual(otherInfo)
       expect(new Date(response.request.yar.get(constants.redisKeys.SUBMISSION_TIMESTAMP))).toBeInstanceOf(Date)
-    })
-    it('Should return view and error message if no description is provided', async () => {
-      const options = {
-        url,
-        payload: {}
-      }
-      const response = await submitPostRequest(options, constants.statusCodes.OK)
-      expect(sendMessage).toHaveBeenCalledTimes(0)
-      expect(response.payload).toContain('There is a problem')
-      expect(response.payload).toContain('Enter a description of the pollution')
     })
     it('Should error if validatePayload fails', async () => {
       const { submitPostRequest } = await import('../../../__test-helpers__/server.js')
