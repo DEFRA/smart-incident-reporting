b35056b66c93974f628bf47bea8b27af644be2ca
Tedd <teddmason@gmail.com>
2024-03-22 11:07:21 +0000

    SIR-676: feedback form work (#88)
    * feedback form work
    
    * sorting test coverage

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -3,16 +3,18 @@ import constants from './constants.js'
 import Ajv from 'ajv'
 import addFormats from 'ajv-formats'
 
-const jsonSchema = JSON.parse(fs.readFileSync('./server/schemas/sirp-schema.json'))
+const sirpSchema = JSON.parse(fs.readFileSync('./server/schemas/sirp-schema.json'))
+const feedbackSchema = JSON.parse(fs.readFileSync('./server/schemas/feedback-schema.json'))
 
 const getErrorSummary = () => {
   return JSON.parse(JSON.stringify(constants.errorSummary))
 }
 
-const validatePayload = payload => {
+const validatePayload = (payload, feedback = false) => {
+  const schema = !feedback ? sirpSchema : feedbackSchema
   const ajv = new Ajv({ strict: false })
   addFormats(ajv)
-  const valid = ajv.validate(jsonSchema, payload)
+  const valid = ajv.validate(schema, payload)
   if (!valid) {
     console.error(ajv.errors)
   }
