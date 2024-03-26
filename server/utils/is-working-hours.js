82e70411112978111b959488a5c982b7ac2ecc95
Tedd Mason <teddmason@gmail.com>
2024-03-26 14:47:27 +0000

    linting

--- server/utils/is-working-hours.js
+++ server/utils/is-working-hours.js
@@ -7,24 +7,24 @@ const isWorkingHours = async (dateToTest = new Date()) => {
     if (config.serviceAvailableCron === '* * * * * *') {
       return true
     }
-  
+
     // Get bank holiday data
     const { payload } = await Wreck.get('https://www.gov.uk/bank-holidays.json', {
       json: true
     })
-  
+
     const dateString = dateToTest.toLocaleDateString('en-GB', { timeZone: 'Europe/London' })
-  
+
     const bankHoliday = payload['england-and-wales'].events.find(event => dateString === new Date(event.date).toLocaleDateString('en-GB', { timeZone: 'Europe/London' }))
-  
+
     if (bankHoliday) {
       return false
     }
-  
+
     const interval = parser.parseExpression(config.serviceAvailableCron, {
       currentDate: dateToTest
     })
-  
+
     return interval.fields.dayOfWeek.includes(dateToTest.getDay()) && interval.fields.hour.includes(dateToTest.getHours())
   } catch (err) {
     console.error(err)
