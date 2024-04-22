8182c83ab1d6a0de46c472945d1b605d98d5a21c
Tedd Mason <teddmason@gmail.com>
2024-04-22 13:20:46 +0100

    fixing linting

--- server/routes/__tests__/home.spec.js
+++ server/routes/__tests__/home.spec.js
@@ -2,7 +2,6 @@ import { submitGetRequest, submitPostRequest } from '../../__test-helpers__/serv
 import constants from '../../utils/constants.js'
 const url = '/'
 
-
 // As the mocked return for this default es6 module export gets cached, separate test file
 // named home-unavailable.spec.js tests the outside of working hours logic
 jest.mock('../../utils/is-working-hours', () => ({
