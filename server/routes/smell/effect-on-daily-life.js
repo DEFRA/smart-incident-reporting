823ffee336effc64556d188b429ea7680e2edd2f
Tedd Mason <teddmason@gmail.com>
2024-05-24 08:58:15 +0100

    removing accidental commented code

--- server/routes/smell/effect-on-daily-life.js
+++ server/routes/smell/effect-on-daily-life.js
@@ -7,65 +7,6 @@ const key = constants.redisKeys.SMELL_EFFECT_ON_DAILY_LIFE
 const view = constants.views.SMELL_EFFECT_ON_DAILY_LIFE
 const route = constants.routes.SMELL_EFFECT_ON_HEALTH
 
-// const baseAnswer = {
-//   questionId: question.questionId,
-//   questionAsked: question.text,
-//   questionResponse: true
-// }
-
-// const handlers = {
-//   get: async (request, h) => {
-//     const onGoingAnswer = request.yar.get(constants.redisKeys.SMELL_ONGOING)
-//     const onGoing = onGoingAnswer && onGoingAnswer[0].answerId === questionSets.SMELL.questions.SMELL_ONGOING.answers.yes.answerId
-//     return h.view(constants.views.SMELL_EFFECT_ON_DAILY_LIFE, {
-//       ...getContext(),
-//       onGoing
-//     })
-//   },
-//   post: async (request, h) => {
-//     request.yar.set(constants.redisKeys.SMELL_EFFECT_ON_DAILY_LIFE, buildAnswers(request.payload))
-
-//     return h.redirect(constants.routes.SMELL_EFFECT_ON_HEALTH)
-//   }
-// }
-
-// const getContext = () => {
-//   return {
-//     question
-//   }
-// }
-
-// const buildAnswers = payload => {
-//   const answers = []
-//   let { effect, otherDetails } = payload
-
-//   if (effect && !Array.isArray(effect)) {
-//     effect = [effect]
-//   }
-
-//   effect?.forEach(item => {
-//     answers.push({
-//       ...baseAnswer,
-//       answerId: Number(item)
-//     })
-//     if (Number(item) === question.answers.other.answerId) {
-//       answers.push({
-//         ...baseAnswer,
-//         answerId: question.answers.otherDetails.answerId,
-//         otherDetails
-//       })
-//     }
-//   })
-//   if (answers.length === 0) {
-//     answers.push({
-//       ...baseAnswer,
-//       answerId: question.answers.noActions.answerId
-//     })
-//   }
-
-//   return answers
-// }
-
 export default [
   {
     method: 'GET',
