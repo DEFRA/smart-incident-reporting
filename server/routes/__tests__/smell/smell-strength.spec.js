574633a5fc4a10f6fd5d5601c18986177d5d88d2
Tedd Mason <teddmason@gmail.com>
2024-09-12 08:39:18 +0100

    smell-strength

--- server/routes/__tests__/smell/smell-strength.spec.js
+++ server/routes/__tests__/smell/smell-strength.spec.js
@@ -2,8 +2,8 @@ import { submitGetRequest, submitPostRequest } from '../../../__test-helpers__/s
 import { questionSets } from '../../../utils/question-sets.js'
 import constants from '../../../utils/constants.js'
 
-const url = constants.routes.SMELL_STRENGTH
-const question = questionSets.SMELL.questions.SMELL_STRENGTH
+const url = constants.routes.SMELL_SMELL_STRENGTH
+const question = questionSets.SMELL.questions.SMELL_SMELL_STRENGTH
 const header = question.text
 const baseAnswer = {
   questionId: question.questionId,
@@ -15,18 +15,18 @@ describe(url, () => {
   describe('GET', () => {
     it(`Should return success response and correct view for ${url} if current smell`, async () => {
       const sessionData = {
-        'smell/ongoing': [{
-          questionId: questionSets.SMELL.questions.SMELL_ONGOING.questionId,
-          answerId: questionSets.SMELL.questions.SMELL_ONGOING.answers.yes.answerId
+        'smell/current': [{
+          questionId: questionSets.SMELL.questions.SMELL_CURRENT.questionId,
+          answerId: questionSets.SMELL.questions.SMELL_CURRENT.answers.yes.answerId
         }]
       }
       await submitGetRequest({ url }, header, constants.statusCodes.OK, sessionData)
     })
     it(`Should return success response and correct view for ${url} if past smell`, async () => {
       const sessionData = {
-        'smell/ongoing': [{
-          questionId: questionSets.SMELL.questions.SMELL_ONGOING.questionId,
-          answerId: questionSets.SMELL.questions.SMELL_ONGOING.answers.no.answerId
+        'smell/current': [{
+          questionId: questionSets.SMELL.questions.SMELL_CURRENT.questionId,
+          answerId: questionSets.SMELL.questions.SMELL_CURRENT.answers.no.answerId
         }]
       }
       const wasHeader = header.replace('is', 'was')
@@ -35,35 +35,7 @@ describe(url, () => {
   })
 
   describe('POST', () => {
-    it('Happy Strong and continues to SMELL_EFFECT_ON_ACTIVITY', async () => {
-      const options = {
-        url,
-        payload: {
-          answerId: question.answers.strong.answerId
-        }
-      }
-      const response = await submitPostRequest(options)
-      expect(response.headers.location).toEqual(constants.routes.SMELL_EFFECT_ON_ACTIVITY)
-      expect(response.request.yar.get(constants.redisKeys.SMELL_STRENGTH)).toEqual([{
-        ...baseAnswer,
-        answerId: question.answers.strong.answerId
-      }])
-    })
-    it('Happy persistent and continues to SMELL_EFFECT_ON_ACTIVITY', async () => {
-      const options = {
-        url,
-        payload: {
-          answerId: question.answers.persistent.answerId
-        }
-      }
-      const response = await submitPostRequest(options)
-      expect(response.headers.location).toEqual(constants.routes.SMELL_EFFECT_ON_ACTIVITY)
-      expect(response.request.yar.get(constants.redisKeys.SMELL_STRENGTH)).toEqual([{
-        ...baseAnswer,
-        answerId: question.answers.persistent.answerId
-      }])
-    })
-    it('Happy faint and continues to SMELL_EFFECT_ON_ACTIVITY', async () => {
+    it('Happy Faint and continues to SMELL_INDOORS', async () => {
       const options = {
         url,
         payload: {
@@ -71,31 +43,59 @@ describe(url, () => {
         }
       }
       const response = await submitPostRequest(options)
-      expect(response.headers.location).toEqual(constants.routes.SMELL_EFFECT_ON_ACTIVITY)
-      expect(response.request.yar.get(constants.redisKeys.SMELL_STRENGTH)).toEqual([{
+      expect(response.headers.location).toEqual(constants.routes.SMELL_INDOORS)
+      expect(response.request.yar.get(constants.redisKeys.SMELL_SMELL_STRENGTH)).toEqual([{
         ...baseAnswer,
         answerId: question.answers.faint.answerId
       }])
     })
-    it('Happy very faint and continues to SMELL_EFFECT_ON_ACTIVITY', async () => {
+    it('Happy distinct and continues to SMELL_INDOORS', async () => {
       const options = {
         url,
         payload: {
-          answerId: question.answers.veryFaint.answerId
+          answerId: question.answers.distinct.answerId
         }
       }
       const response = await submitPostRequest(options)
-      expect(response.headers.location).toEqual(constants.routes.SMELL_EFFECT_ON_ACTIVITY)
-      expect(response.request.yar.get(constants.redisKeys.SMELL_STRENGTH)).toEqual([{
+      expect(response.headers.location).toEqual(constants.routes.SMELL_INDOORS)
+      expect(response.request.yar.get(constants.redisKeys.SMELL_SMELL_STRENGTH)).toEqual([{
         ...baseAnswer,
-        answerId: question.answers.veryFaint.answerId
+        answerId: question.answers.distinct.answerId
       }])
     })
-    it('Sad rejects empty payload with ongoing smell', async () => {
+    it('Happy strong and continues to SMELL_INDOORS', async () => {
+      const options = {
+        url,
+        payload: {
+          answerId: question.answers.strong.answerId
+        }
+      }
+      const response = await submitPostRequest(options)
+      expect(response.headers.location).toEqual(constants.routes.SMELL_INDOORS)
+      expect(response.request.yar.get(constants.redisKeys.SMELL_SMELL_STRENGTH)).toEqual([{
+        ...baseAnswer,
+        answerId: question.answers.strong.answerId
+      }])
+    })
+    it('Happy very strong and continues to SMELL_INDOORS', async () => {
+      const options = {
+        url,
+        payload: {
+          answerId: question.answers.veryStrong.answerId
+        }
+      }
+      const response = await submitPostRequest(options)
+      expect(response.headers.location).toEqual(constants.routes.SMELL_INDOORS)
+      expect(response.request.yar.get(constants.redisKeys.SMELL_SMELL_STRENGTH)).toEqual([{
+        ...baseAnswer,
+        answerId: question.answers.veryStrong.answerId
+      }])
+    })
+    it('Sad rejects empty payload with current smell', async () => {
       const sessionData = {
-        'smell/ongoing': [{
-          questionId: questionSets.SMELL.questions.SMELL_ONGOING.questionId,
-          answerId: questionSets.SMELL.questions.SMELL_ONGOING.answers.yes.answerId
+        'smell/current': [{
+          questionId: questionSets.SMELL.questions.SMELL_CURRENT.questionId,
+          answerId: questionSets.SMELL.questions.SMELL_CURRENT.answers.yes.answerId
         }]
       }
       const options = {
@@ -108,9 +108,9 @@ describe(url, () => {
     })
     it('Sad rejects empty payload with past smell', async () => {
       const sessionData = {
-        'smell/ongoing': [{
-          questionId: questionSets.SMELL.questions.SMELL_ONGOING.questionId,
-          answerId: questionSets.SMELL.questions.SMELL_ONGOING.answers.no.answerId
+        'smell/current': [{
+          questionId: questionSets.SMELL.questions.SMELL_CURRENT.questionId,
+          answerId: questionSets.SMELL.questions.SMELL_CURRENT.answers.no.answerId
         }]
       }
       const options = {
