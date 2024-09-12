08623b8c2ab1cf96ddef53ea8b8ad47a19951a4f
sujithvg <sujithvg@users.noreply.github.com>
2024-09-12 12:21:58 +0100

    Updated files to add back link

--- server/routes/smell/report-local-council.js
+++ server/routes/smell/report-local-council.js
@@ -2,16 +2,7 @@ import constants from '../../utils/constants.js'
 
 const handlers = {
   get: async (_request, h) => {
-    const context = _getContext()
-    return h.view(constants.views.SMELL_REPORT_LOCAL_COUNCIL, {
-      ...context
-    })
-  }
-}
-
-const _getContext = () => {
-  return {
-    hideBackLink: true
+    return h.view(constants.views.SMELL_REPORT_LOCAL_COUNCIL)
   }
 }
 
