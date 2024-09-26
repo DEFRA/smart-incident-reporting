12e4d90b2f8bf3a600f85ea3ddd28ca0c72926c2
sujithvg <sujithvg@users.noreply.github.com>
2024-09-26 09:49:25 +0100

    Removed empty field validation check for postcode (#185)
    * Removed empty field validation check for postcode
    
    * Fixed test case issues
    
    * Fixed test case issues

--- server/routes/smell/source-details.js
+++ server/routes/smell/source-details.js
@@ -75,18 +75,11 @@ const validatePayload = payload => {
         href: '#sourceTown'
       })
     }
-    if (!payload.sourcePostcode) {
-      errorSummary.errorList.push({
-        text: 'Enter a postcode',
-        href: '#sourcePostcode'
-      })
-    } else if (!postcodeRegExp.test(payload.sourcePostcode)) {
+    if ((payload.sourcePostcode?.length > 0) && (!postcodeRegExp.test(payload.sourcePostcode))) {
       errorSummary.errorList.push({
         text: 'Enter a full UK postcode',
         href: '#sourcePostcode'
       })
-    } else {
-      // do nothing
     }
   } else {
     // do nothing
