1d0509229e6c8137490bf6f0b92645cc66793f4b
Tedd Mason <teddmason@gmail.com>
2024-10-16 08:40:37 +0100

    logging update

--- server/plugins/logging.js
+++ server/plugins/logging.js
@@ -17,6 +17,7 @@ export default {
       '/public/js/locationMap.js',
       '/public/govuk-frontend.min.js',
       '/public/images/favicon.svg',
+      '/public/images/favicon.ico',
       '/public/images/govuk-crest.svg',
       '/public/manifest.json',
       '/public/fonts/light-94a07e06a1-v2.woff2',
