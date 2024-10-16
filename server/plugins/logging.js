bd1c49e478a08f1342c5ec762df90eb335ef5464
Tedd Mason <teddmason@gmail.com>
2024-10-16 08:10:53 +0100

    SIR-1075 Updating logging

--- server/plugins/logging.js
+++ server/plugins/logging.js
@@ -5,6 +5,22 @@ export default {
   plugin: HapiPino,
   options: {
     logPayload: true,
-    level: config.logLevel
+    level: config.logLevel,
+    redact: {
+      paths: ['req.headers.authorization', 'req.headers.cookie', 'res.headers'],
+      remove: true
+    },
+    ignorePaths: [
+      '/public/stylesheets/application.css',
+      '/public/js/core.js',
+      '/public/js/cookies.js',
+      '/public/js/locationMap.js',
+      '/public/govuk-frontend.min.js',
+      '/public/images/favicon.svg',
+      '/public/images/govuk-crest.svg',
+      '/public/manifest.json',
+      '/public/fonts/light-94a07e06a1-v2.woff2',
+      '/public/fonts/bold-b542beb274-v2.woff2'
+    ]
   }
 }
