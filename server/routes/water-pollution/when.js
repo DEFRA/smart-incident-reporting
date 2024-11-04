c52290471427a5158c49da298daaf5fe764d50b3
Tedd <teddmason@gmail.com>
2024-11-04 08:21:43 +0000

    SIR-1101 - Updating water pollution to match smell date time format (#216)

--- server/routes/water-pollution/when.js
+++ server/routes/water-pollution/when.js
@@ -1,4 +1,5 @@
 import constants from '../../utils/constants.js'
+import { getErrorSummary } from '../../utils/helpers.js'
 import {
   dateValidateAndError,
   fieldErrorClasses,
@@ -18,24 +19,53 @@ const handlers = {
   },
   post: async (request, h) => {
     const validateAndError = dateValidateAndError()
+    if (!request.payload.current) {
+      const errors = getErrorSummary()
+      errors.errorList.push({
+        text: 'Select when you saw the pollution',
+        href: '#current'
+      })
+      return h.view(constants.views.WATER_POLLUTION_WHEN, {
+        errorSummary: errors,
+        validateAndError,
+        fieldErrorClasses,
+        getDateErrors,
+        getTimeErrors
+      })
+    }
+
     const payload = {
-      day: request.payload['date-day'],
-      month: request.payload['date-month'],
-      year: request.payload['date-year'],
-      hour: request.payload.hour,
-      minute: request.payload.minute,
-      period: request.payload.period
+      day: request.payload.current === '2' ? request.payload['date-day'] : undefined,
+      month: request.payload.current === '2' ? request.payload['date-month'] : undefined,
+      year: request.payload.current === '2' ? request.payload['date-year'] : undefined,
+      hour: request.payload.hour[Number(request.payload.current)],
+      minute: request.payload.minute[Number(request.payload.current)],
+      period: request.payload.period[Number(request.payload.current)]
+    }
+    // Fill in today or yesterday dates
+    if (request.payload.current !== '2') {
+      const date = new Date()
+      if (request.payload.current === '1') {
+        date.setDate(date.getDate() - 1)
+      }
+      payload.day = date.getDate().toString()
+      payload.month = (date.getMonth() + 1).toString()
+      payload.year = date.getFullYear().toString()
     }
 
     // validate payload for errors
     const { errorSummary, dateTime } = validatePayload(payload, validateAndError)
     if (errorSummary.errorList.length > 0) {
+      errorSummary.errorList.forEach(item => {
+        item.href = item.href.indexOf('date') === -1 ? `${item.href}-${request.payload.current}` : item.href
+      })
       return h.view(constants.views.WATER_POLLUTION_WHEN, {
         errorSummary,
         validateAndError,
         fieldErrorClasses,
         getDateErrors,
-        getTimeErrors
+        getTimeErrors,
+        current: request.payload.current
       })
     }
 
