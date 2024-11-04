50a18ca11244ac8e26f7dba589ce33efeb577876
Tedd <teddmason@gmail.com>
2024-11-04 14:49:28 +0000

    SIR-890 change location from CYA (#212)
    * SIR-890 change location from CYA
    
    * removing .only from unit tests
    
    * sonarcloud

--- server/routes/water-pollution/location-map.js
+++ server/routes/water-pollution/location-map.js
@@ -10,9 +10,9 @@ const baseAnswer = {
 }
 
 const handlers = {
-  get: async (_request, h) => {
+  get: async (request, h) => {
     return h.view(constants.views.WATER_POLLUTION_LOCATION_MAP, {
-      ...getContext()
+      ...getContext(request)
     })
   },
   post: async (request, h) => {
@@ -22,21 +22,28 @@ const handlers = {
 
     if (!point || point.length === 0) {
       return h.view(constants.views.WATER_POLLUTION_LOCATION_MAP, {
-        ...getContext(),
+        ...getContext(request),
         noPoint: true
       })
     }
 
-    request.yar.set(constants.redisKeys.WATER_POLLUTION_LOCATION_MAP, buildAnswers(point))
+    request.yar.set(question.key, buildAnswers(point))
 
     // handle redirects
-    return h.redirect(constants.routes.WATER_POLLUTION_WHEN)
+    return h.redirect(request.yar.get(constants.redisKeys.REFERER) || constants.routes.WATER_POLLUTION_WHEN)
   }
 }
 
-const getContext = () => {
+const getContext = request => {
+  const location = request.yar.get(constants.redisKeys.WATER_POLLUTION_LOCATION_MAP)
+  const locationAnswer = location && {
+    point: [Number(location[1].otherDetails), Number(location[2].otherDetails)],
+    zoom: 10
+  }
+
   return {
-    question
+    question,
+    locationAnswer
   }
 }
 
--- server/utils/template-helpers.js
+++ server/utils/template-helpers.js
@@ -6,7 +6,11 @@ const findErrorMessageById = (errorSummary, id) => {
 
 const getAnswer = (answers, answerId) => {
   const answer = answers?.find(item => item.answerId === answerId)
-  return answer?.otherDetails ? answer.otherDetails : !!answer
+  if (answer?.otherDetails) {
+    return answer.otherDetails
+  } else {
+    return answer ? true : ''
+  }
 }
 
 export {
