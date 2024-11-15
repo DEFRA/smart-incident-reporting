46b49c9201acf6fef594ba0d3ce709fc6cb0bba1
sujithvg <sujithvg@users.noreply.github.com>
2024-11-15 17:56:25 +0000

    Technical implementation for SIR-894

--- server/routes/water-pollution/effect-on-wildlife.js
+++ server/routes/water-pollution/effect-on-wildlife.js
@@ -11,57 +11,70 @@ const baseAnswer = {
 }
 
 const handlers = {
-  get: async (_request, h) => {
+  get: async (request, h) => {
     return h.view(constants.views.WATER_POLLUTION_EFFECT_ON_WILDLIFE, {
-      ...getContext()
+      ...getContext(request)
     })
   },
   post: async (request, h) => {
+    // get payload
+    let { answerId, yesDetails } = request.payload
+
     // validate payload
-    const errorSummary = validatePayload(request.payload)
+    const errorSummary = validatePayload(answerId)
     if (errorSummary.errorList.length > 0) {
       return h.view(constants.views.WATER_POLLUTION_EFFECT_ON_WILDLIFE, {
-        ...getContext(),
-        errorSummary,
-        ...request.payload
+        ...getContext(request),
+        errorSummary
       })
     }
 
-    request.yar.set(constants.redisKeys.WATER_POLLUTION_EFFECT_ON_WILDLIFE, buildAnswers(request.payload))
+    // convert answerId to number
+    answerId = Number(answerId)
 
-    return h.redirect(constants.routes.WATER_POLLUTION_OTHER_INFORMATION)
+    // set answer in session
+    request.yar.set(constants.redisKeys.WATER_POLLUTION_EFFECT_ON_WILDLIFE, buildAnswers(answerId, yesDetails))
+
+    // handle redirection
+    return h.redirect(request.yar.get(constants.redisKeys.REFERER) || constants.routes.WATER_POLLUTION_OTHER_INFORMATION)
   }
 }
 
-const getContext = () => {
+const getContext = (request) => {
+  const answers = request.yar.get(question.key)
+  console.log('Data for answers', answers)
   return {
-    question
+    question,
+    answers
   }
 }
 
-const validatePayload = payload => {
+const validatePayload = answerId => {
   const errorSummary = getErrorSummary()
-  if (!payload.effectOnWildlife) {
+  if (!answerId) {
     errorSummary.errorList.push({
       text: 'Select yes if you\'ve seen dead or distressed fish or other animals nearby',
-      href: '#effectOnWildlife'
+      href: '#answerId'
     })
   }
   return errorSummary
 }
 
-const buildAnswers = payload => {
-  const answers = [{
+const buildAnswers = (answerId, yesDetails) => {
+  const answers = []
+  answers.push({
     ...baseAnswer,
-    answerId: payload.effectOnWildlife === 'yes' ? question.answers.yes.answerId : question.answers.no.answerId
-  }]
-  if (payload.effectOnWildlife === 'yes' && payload.yesDetails) {
+    answerId
+  })
+
+  if (answerId === question.answers.yes.answerId && yesDetails) {
     answers.push({
       ...baseAnswer,
       answerId: question.answers.yesDetails.answerId,
-      otherDetails: payload.yesDetails
+      otherDetails: yesDetails
     })
   }
+
   return answers
 }
 
