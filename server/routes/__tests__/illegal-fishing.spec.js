c895003021ae1d26da8c9f4a0f8dd2bb76f3db25
sujithvg <sujithvg@users.noreply.github.com>
2025-02-12 16:40:15 +0000

    Fixed sonar issues - Part 2

---
+++ server/routes/__tests__/illegal-fishing.spec.js
@@ -0,0 +1,13 @@
+import { submitGetRequest } from '../../__test-helpers__/server.js'
+import constants from '../../utils/constants.js'
+
+const url = constants.routes.ILLEGAL_FISHING
+const header = 'Report someone fishing illegally'
+
+describe(url, () => {
+  describe('GET', () => {
+    it(`Should return success response and correct view for ${url}`, async () => {
+      await submitGetRequest({ url }, header)
+    })
+  })
+})
