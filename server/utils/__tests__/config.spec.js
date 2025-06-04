56acf33fc8deb6f109fa59a2552212fbbfb2687d
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2025-06-05 00:23:06 +0100

    Config tests

--- server/utils/__tests__/config.spec.js
+++ server/utils/__tests__/config.spec.js
@@ -10,4 +10,26 @@ describe('utils/config', () => {
       expect(() => require('../config.js')).toThrow('The server config is invalid. "servicePort" must be a number')
     })
   })
+  it('Should set captcha API and site key if captcha enabled', () => {
+    jest.isolateModules(() => {
+      const apiKey = '1234'
+      const siteKey = 'abcd'
+
+      process.env.CAPTCHA_ENABLED = 'true'
+      process.env.CAPTCHA_API_KEY = apiKey
+      process.env.CAPTCHA_SITE_KEY = siteKey
+
+      const config = require('../config.js')
+      expect(config.default.captchaApiKey).toBe(apiKey)
+      expect(config.default.captchaSiteKey).toBe(siteKey)
+    })
+  })
+  it('Should set captcha API and site key to empty if captcha disabled', () => {
+    jest.isolateModules(() => {
+      process.env.CAPTCHA_ENABLED = 'false'
+      const config = require('../config.js')
+      expect(config.default.captchaApiKey).toBe('')
+      expect(config.default.captchaSiteKey).toBe('')
+    })
+  })
 })
