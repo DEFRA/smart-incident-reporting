5af96be5b326ac5e97ebf3d47ec2207cbea472cd
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2025-06-10 13:52:40 +0100

    Show home route if local home routes registered

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,8 +1,13 @@
-import constants from "../utils/constants.js"
+import constants from '../utils/constants.js'
 
 const handlers = {
   get: (_request, h) => {
-    return h.redirect(constants.urls.GOV_UK_SERVICE_HOME).permanent()
+    if (process.env.REGISTER_START_ROUTES === 'true') {
+      request.yar.reset()
+      return h.view(constants.views.HOME)
+    } else {
+      return h.redirect(constants.urls.GOV_UK_SERVICE_HOME).permanent()
+    }    
   }
 }
 
