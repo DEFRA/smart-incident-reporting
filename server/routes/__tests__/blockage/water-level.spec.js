728248690cb0e5253a1f1b6a3fd2edd1d9afbffe
Sandhya Shet <101565781+Sandhya1804@users.noreply.github.com>
2025-12-12 12:20:20 +0000

    feat/SIR-1871 -Is the water building up behind the blockage (#318)
    * https://eaflood.atlassian.net/browse/SIR-1874
    
    * completed the water-level page

---
+++ server/routes/__tests__/blockage/water-level.spec.js
@@ -0,0 +1,87 @@
+import { submitGetRequest, submitPostRequest } from '../../../__test-helpers__/server.js'
+import { questionSets } from '../../../utils/question-sets.js'
+import constants from '../../../utils/constants.js'
+
+const url = constants.routes.BLOCKAGE_WATER_LEVEL
+const question = questionSets.BLOCKAGE.questions.BLOCKAGE_WATER_LEVEL
+const baseAnswer = {
+  questionId: question.questionId,
+  questionAsked: question.text,
+  questionResponse: true
+}
+
+const sessionData = {
+  'blockage/water-level': [{
+    questionId: baseAnswer.questionId,
+    answerId: question.answers.yes.answerId
+  }]
+}
+
+describe(url, () => {
+  describe('GET', () => {
+    it(`Should return success response and correct view for ${url}`, async () => {
+      const response = await submitGetRequest({ url }, question.text)
+      expect(response.statusCode).toBe(constants.statusCodes.OK)
+    })
+    it(`Should display correct question text for ${url}`, async () => {
+      const response = await submitGetRequest({ url }, question.text)
+      expect(response.payload).toContain(question.text)
+    })
+    it(`Should return success response and correct view for ${url}`, async () => {
+      const response = await submitGetRequest({ url }, question.text, constants.statusCodes.OK, sessionData)
+      expect(response.payload).toContain('<input class="govuk-radios__input" id="answerId" name="answerId" type="radio" value="141" checked>')
+    })
+  })
+  describe('POST', () => {
+    // update redirectTo
+    it.each([
+      { answer: 'yes', description: 'yes', redirectTo: constants.routes.BLOCKAGE_FLOOD_RISK },
+      { answer: 'no', description: 'no', redirectTo: constants.routes.BLOCKAGE_FLOOD_RISK },
+      { answer: 'youDoNotKnow', description: 'you dont know', redirectTo: constants.routes.BLOCKAGE_FLOOD_RISK }
+    ])('Should redirect to blockage/flood-risk when $water-level selected', async ({ answer, redirectTo }) => {
+      const answerId = question.answers[answer].answerId
+      const options = {
+        url,
+        payload: {
+          answerId
+        }
+      }
+      const response = await submitPostRequest(options)
+      expect(response.headers.location).toEqual(redirectTo)
+    })
+    it.each([
+      { answer: 'yes', description: 'yes' },
+      { answer: 'no', description: 'no' },
+      { answer: 'youDoNotKnow', description: 'you dont know' }
+    ])('Should save $water-level answer to session', async ({ answer }) => {
+      const answerId = question.answers[answer].answerId
+      const options = {
+        url,
+        payload: {
+          answerId
+        }
+      }
+      const response = await submitPostRequest(options)
+      expect(response.request.yar.get(constants.redisKeys.BLOCKAGE_WATER_LEVEL)).toEqual([{
+        ...baseAnswer,
+        answerId
+      }])
+    })
+    it('Should return error when no radio selected', async () => {
+      const options = {
+        url,
+        payload: {}
+      }
+      const response = await submitPostRequest(options, constants.statusCodes.OK)
+      expect(response.payload).toContain('There is a problem')
+    })
+    it('Should display correct error message when no radio selected', async () => {
+      const options = {
+        url,
+        payload: {}
+      }
+      const response = await submitPostRequest(options, constants.statusCodes.OK)
+      expect(response.payload).toContain('Select ‘yes’ if water is building up behind the blockage')
+    })
+  })
+})
---
+++ server/routes/blockage/flood-risk.js
@@ -0,0 +1,84 @@
+import constants from '../../utils/constants.js'
+import { getErrorSummary } from '../../utils/helpers.js'
+import { questionSets } from '../../utils/question-sets.js'
+
+const question = questionSets.BLOCKAGE.questions.BLOCKAGE_FLOOD_RISK
+
+const baseAnswer = {
+  questionId: question.questionId,
+  questionAsked: question.text,
+  questionResponse: true
+}
+
+const handlers = {
+  get: async (request, h) => {
+    return h.view(constants.views.BLOCKAGE_FLOOD_RISK, {
+      ...getContext(request)
+    })
+  },
+  post: async (request, h) => {
+    const { answerId: answerIdRaw } = request.payload
+
+    // validate payload
+    const errorSummary = validatePayload(answerIdRaw)
+    if (errorSummary.errorList.length > 0) {
+      return h.view(constants.views.BLOCKAGE_FLOOD_RISK, {
+        ...getContext(request),
+        errorSummary
+      })
+    }
+
+    // convert answerId to number
+    const answerId = Number(answerIdRaw)
+
+    request.yar.set(constants.redisKeys.BLOCKAGE_FLOOD_RISK, buildAnswers(answerId))
+
+    return h.redirect(constants.routes.BLOCKAGE_START)
+
+    // handle redirects
+    // if (answerId === question.answers.alreadyFlooding.answerId || answerId === question.answers.yes.answerId) {
+    //   return h.redirect(constants.routes.BLOCKAGE_AT_RISK_OF_FLOOD)
+    // } else {
+    //   return h.redirect(constants.routes.BLOCKAGE_YOUR_DETAILS)
+    // }
+  }
+}
+
+const getContext = request => {
+  const answers = request.yar.get(question.key)
+  return {
+    question,
+    answers
+  }
+}
+
+const validatePayload = answerId => {
+  const errorSummary = getErrorSummary()
+  if (!answerId) {
+    errorSummary.errorList.push({
+      text: 'Select whether the blockage has been here for some time or you do not know',
+      href: '#answerId'
+    })
+  }
+  return errorSummary
+}
+
+const buildAnswers = (answerId) => {
+  return [{
+    ...baseAnswer,
+    answerId
+  }]
+}
+
+export default [
+  {
+    method: 'GET',
+    path: constants.routes.BLOCKAGE_FLOOD_RISK,
+    handler: handlers.get
+  },
+  {
+    method: 'POST',
+    path: constants.routes.BLOCKAGE_FLOOD_RISK,
+    handler: handlers.post
+  }
+]
