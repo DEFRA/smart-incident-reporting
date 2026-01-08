3b36f400b8b86f33f291eb7e0bce47fc590be230
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2026-01-08 22:07:05 +0000

    * Make sure Blockage journey routes are registered for production as these will appear in next release * Update view compile function to inject service name and URL based on the filename of the template as this tells us which journey the template is for

--- server/plugins/views.js
+++ server/plugins/views.js
@@ -6,6 +6,7 @@ import constants from '../utils/constants.js'
 import fs from 'fs'
 import dirname from '../../dirname.cjs'
 import { findErrorMessageById, getAnswer } from '../utils/template-helpers.js'
+import getServiceDetails from '../utils/get-service-details.js'
 const { version } = JSON.parse(fs.readFileSync('./package.json'))
 const analyticsAccount = config.analyticsAccount
 
@@ -16,7 +17,8 @@ export default {
       html: {
         compile: (src, options) => {
           const template = nunjucks.compile(src, options.environment)
-          return context => template.render(context)
+          const serviceNameAndURL = getServiceDetails(options.filename)
+          return context => template.render({ ...context, ...serviceNameAndURL })
         },
         prepare: (options, next) => {
           const env = options.compileOptions.environment = nunjucks.configure(options.path, {
@@ -43,9 +45,6 @@ export default {
       env: config.env,
       assetPath: '/public',
       govUkHome: constants.urls.GOV_UK_HOME,
-      serviceNameUrl: constants.urls.GOV_UK_SERVICE_HOME,
-      serviceName: 'Report an environmental problem',
-      titleSuffix: ' - report an environmental problem - GOV.UK',
       analyticsAccount
     }
   }
