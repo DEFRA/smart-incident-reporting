024baaad071057e6ff15b6ee15903668f1ebff4f
Shet, Sandhya <Sandhya.Shet@defra.gov.uk>
2026-02-13 11:12:24 +0000

    Implemented the uplload photo details on report sent page

--- server/routes/report-sent.js
+++ server/routes/report-sent.js
@@ -2,14 +2,51 @@ import constants from '../utils/constants.js'
 
 const handlers = {
   get: async (request, h) => {
-    const questionSetID = request.yar.get(constants.redisKeys.QUESTION_SET_ID)
+    const questionSetID = request.yar.get(constants.redisKeys.QUESTION_SET_ID)    
+    const photoUploadDetails = getPhotoUploadDetails(request, questionSetID)    
     request.yar.reset()
-    request.yar.set(constants.redisKeys.QUESTION_SET_ID, questionSetID)
-    const context = _getContext()
+    const context = _getContext()    
     return h.view(constants.views.REPORT_SENT, {
-      ...context
+      ...context, photoUploadDetails
     })
   }
+}
+
+function getPhotoUploadDetails (request, questionSetID) {
+  const keyMap = {
+    100: {
+      contact: constants.redisKeys.WATER_POLLUTION_CONTACT_DETAILS,
+      images: constants.redisKeys.WATER_POLLUTION_IMAGES_OR_VIDEO
+    },
+    200: {
+      contact: constants.redisKeys.SMELL_CONTACT_DETAILS,
+      images: constants.redisKeys.SMELL_IMAGES_OR_VIDEO
+    },
+    300: {
+      contact: constants.redisKeys.BLOCKAGE_CONTACT_DETAILS,
+      images: constants.redisKeys.BLOCKAGE_IMAGES_OR_VIDEO
+    },
+    1800: {
+      contact: constants.redisKeys.ILLEGAL_FISHING_CONTACT_DETAILS,
+      images: constants.redisKeys.ILLEGAL_FISHING_IMAGES_OR_VIDEO
+    }
+  }
+
+  const keys = keyMap[questionSetID]
+
+  if (!keys) {
+    return {
+      userAgreedForImages: false,
+      reportersEmail: ''
+    }
+  }
+  const reportersEmail =
+    request.yar.get(keys.contact).reporterEmailAddress
+
+  const userAgreedForImages =
+    request.yar.get(keys.images)[0].questionResponse
+
+  return { userAgreedForImages, reportersEmail }
 }
 
 const _getContext = () => {
