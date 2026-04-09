55f75e2df4d755ba6f8fb55f5f29551d939d8c23
Scott <scott.dormand@defra.gov.uk>
2026-04-09 10:55:40 +0100

    update cache

--- server/routes/__tests__/report-sent.spec.js
+++ server/routes/__tests__/report-sent.spec.js
@@ -1,13 +1,43 @@
 import { submitGetRequest } from '../../__test-helpers__/server.js'
 import constants from '../../utils/constants.js'
+import reportSentRoutes from '../report-sent.js'
 
 const url = constants.routes.REPORT_SENT
 const header = 'Report sent'
+const submissionTimestamp = '2026-04-09T09:00:00.000Z'
+const sessionId = 'test-session-id'
 
 describe(url, () => {
   describe('GET', () => {
     it(`Should return success response and correct view for ${url}`, async () => {
       await submitGetRequest({ url }, header)
+    })
+
+    it.each([
+      { questionSetID: 100, expectedJourney: 'water pollution' },
+      { questionSetID: 200, expectedJourney: 'smell' },
+      { questionSetID: 300, expectedJourney: 'blockage' },
+      { questionSetID: 1800, expectedJourney: 'illegal fishing' }
+    ])('should cache journey "$expectedJourney" for questionSetID $questionSetID', async ({ questionSetID, expectedJourney }) => {
+      const set = jest.fn()
+      await reportSentRoutes[0].handler({
+        yar: {
+          get: jest.fn(key => ({
+            [constants.redisKeys.QUESTION_SET_ID]: questionSetID,
+            [constants.redisKeys.SUBMISSION_TIMESTAMP]: submissionTimestamp
+          }[key])),
+          id: sessionId,
+          reset: jest.fn()
+        },
+        server: {
+          cache: jest.fn(() => ({ set }))
+        }
+      }, { view: jest.fn() })
+
+      expect(set).toHaveBeenCalledWith(sessionId, {
+        journey: expectedJourney,
+        dateTime: submissionTimestamp
+      })
     })
   })
 })
--- server/routes/report-sent.js
+++ server/routes/report-sent.js
@@ -1,8 +1,32 @@
 import constants from '../utils/constants.js'
 
+const expire = 72 * 60 * 60 * 1000
+
+const journeyMap = {
+  100: 'water pollution',
+  200: 'smell',
+  300: 'blockage',
+  1800: 'illegal fishing'
+}
+
 const handlers = {
   get: async (request, h) => {
-    request.yar.get(constants.redisKeys.QUESTION_SET_ID)
+    const questionSetID = request.yar.get(constants.redisKeys.QUESTION_SET_ID)
+    const submissionTimestamp = request.yar.get(constants.redisKeys.SUBMISSION_TIMESTAMP)
+    const journey = journeyMap[questionSetID]
+    const sessionId = request.yar.id
+
+    const mediaUploadCache = request.server.cache({
+      cache: 'redis_cache',
+      segment: 'media-upload',
+      expiresIn: expire
+    })
+
+    await mediaUploadCache.set(sessionId, {
+      journey,
+      dateTime: submissionTimestamp
+    })
+
     request.yar.reset()
     const context = _getContext()
     return h.view(constants.views.REPORT_SENT, {
