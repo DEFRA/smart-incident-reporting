c99cf2c8b8763a41633cc5c6a46107edb9741b1f
Scott <scott.dormand@defra.gov.uk>
2026-04-13 09:32:11 +0100

    display content on report sent

--- server/routes/report-sent.js
+++ server/routes/report-sent.js
@@ -1,7 +1,9 @@
 import constants from '../utils/constants.js'
+import { questionSets } from '../utils/question-sets.js'
 // import config from '../utils/config.js'
 
 const expire = 72 * 60 * 60 * 1000
+const mediaUploadLink = 'https://sir-uploader-dev1.azure.defra.cloud/upload-photo'
 
 const journeyMap = {
   100: 'water pollution',
@@ -10,12 +12,48 @@ const journeyMap = {
   1800: 'illegal fishing'
 }
 
+const journeyConfigMap = {
+  100: {
+    contactDetailsKey: constants.redisKeys.WATER_POLLUTION_CONTACT_DETAILS,
+    imagesOrVideoKey: constants.redisKeys.WATER_POLLUTION_IMAGES_OR_VIDEO,
+    imagesQuestion: questionSets.WATER_POLLUTION.questions.WATER_POLLUTION_IMAGES_OR_VIDEO
+  },
+  200: {
+    contactDetailsKey: constants.redisKeys.SMELL_CONTACT_DETAILS,
+    imagesOrVideoKey: constants.redisKeys.SMELL_IMAGES_OR_VIDEO,
+    imagesQuestion: questionSets.SMELL.questions.SMELL_IMAGES_OR_VIDEO
+  },
+  300: {
+    contactDetailsKey: constants.redisKeys.BLOCKAGE_CONTACT_DETAILS,
+    imagesOrVideoKey: constants.redisKeys.BLOCKAGE_IMAGES_OR_VIDEO,
+    imagesQuestion: questionSets.BLOCKAGE.questions.BLOCKAGE_IMAGES_OR_VIDEO
+  },
+  1800: {
+    contactDetailsKey: constants.redisKeys.ILLEGAL_FISHING_CONTACT_DETAILS,
+    imagesOrVideoKey: constants.redisKeys.ILLEGAL_FISHING_IMAGES_OR_VIDEO,
+    imagesQuestion: questionSets.ILLEGAL_FISHING.questions.ILLEGAL_FISHING_IMAGES_OR_VIDEO
+  }
+}
+
 const handlers = {
   get: async (request, h) => {
     const questionSetID = request.yar.get(constants.redisKeys.QUESTION_SET_ID)
     const submissionTimestamp = request.yar.get(constants.redisKeys.SUBMISSION_TIMESTAMP)
     const journey = journeyMap[questionSetID]
     const sessionId = request.yar.id
+    const journeyConfig = journeyConfigMap[questionSetID]
+
+    const contactDetails = journeyConfig
+      ? request.yar.get(journeyConfig.contactDetailsKey)
+      : null
+    const imagesOrVideoAnswer = journeyConfig
+      ? request.yar.get(journeyConfig.imagesOrVideoKey)
+      : null
+
+    const reportersEmail = contactDetails?.reporterEmailAddress || ''
+    const userAgreedForImages = journeyConfig
+      ? imagesOrVideoAnswer?.[0]?.answerId === journeyConfig.imagesQuestion.answers.yes.answerId
+      : false
 
     const mediaUploadCache = request.server.cache({
       cache: 'redis_cache',
@@ -29,18 +67,21 @@ const handlers = {
     })
 
     request.yar.reset()
-    const context = _getContext()
-    return h.view(constants.views.REPORT_SENT, {
-      ...context,
-      mediaUploadLink: 'https://sir-uploader-dev1.azure.defra.cloud/upload-photo'
+    const context = _getContext({
+      reportersEmail,
+      userAgreedForImages,
+      mediaUploadLink
       // mediaUploadLink: config.mediaUploadUrl
     })
+    return h.view(constants.views.REPORT_SENT, context)
   }
 }
 
-const _getContext = () => {
+const _getContext = (photoUploadDetails) => {
   return {
-    hideBackLink: true
+    hideBackLink: true,
+    photoUploadDetails
+    // mediaUploadLink: config.mediaUploadUrl
   }
 }
 
