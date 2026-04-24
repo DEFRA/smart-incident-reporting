9158b60c1b93c0a5d412b128a21fa82bea6a9113
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2026-04-24 14:32:34 +0100

    media upload testing

--- server/utils/config.js
+++ server/utils/config.js
@@ -31,7 +31,7 @@ const schema = Joi.object().keys({
   captchaSiteKey: Joi.string().allow(''),
   captchaBypassKey: Joi.string(),
   // mediaUploadUrl: Joi.string().uri({ scheme: ['http', 'https'] }).allow('').default('')
-  mediaUploadUrl: Joi.string().uri({ scheme: ['http', 'https'] }).allow('').default('https://sir-uploader-pre1.azure.defra.cloud')
+  mediaUploadUrl: Joi.string().uri({ scheme: ['http', 'https'] }).allow('').default('https://sir-uploader-tst1.azure.defra.cloud')
 })
 
 const captchaEnabled = getBoolean(process.env.CAPTCHA_ENABLED)
