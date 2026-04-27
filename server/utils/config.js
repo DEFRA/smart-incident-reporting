6a36959b70ec9c6477fa3a9fd8d7aea660bbda17
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2026-04-27 12:00:18 +0100

    Use app gateway address

--- server/utils/config.js
+++ server/utils/config.js
@@ -31,7 +31,7 @@ const schema = Joi.object().keys({
   captchaSiteKey: Joi.string().allow(''),
   captchaBypassKey: Joi.string(),
   // mediaUploadUrl: Joi.string().uri({ scheme: ['http', 'https'] }).allow('').default('')
-  mediaUploadUrl: Joi.string().uri({ scheme: ['http', 'https'] }).allow('').default('https://tstsirwebaw1406.azurewebsites.net')
+  mediaUploadUrl: Joi.string().uri({ scheme: ['http', 'https'] }).allow('').default('https://sir-uploader-tst1.azure.defra.cloud')
 })
 
 const captchaEnabled = getBoolean(process.env.CAPTCHA_ENABLED)
