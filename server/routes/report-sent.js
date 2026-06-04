519196fe916d5e4b839e8ef0159e38ae96bfd1be
Scott <scott.dormand@defra.gov.uk>
2026-06-04 09:02:33 +0100

    test cache

--- server/routes/report-sent.js
+++ server/routes/report-sent.js
@@ -65,7 +65,9 @@ const handlers = {
       await request.server.app.mediaUploadCache.set(sessionId, {
         journey,
         dateTime: submissionTimestamp
-      })
+      }),
+      // 168 * 60 * 60 * 1000
+      1 * 60 * 60 * 1000
 
       mediaUploadLink = `${mediaUploadBaseUrl}?sirid=${sessionId}`
     }
