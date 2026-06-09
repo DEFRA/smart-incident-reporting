7f4bb1fab31e27549b1fa65e0f0f64276c1c7053
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2026-06-09 10:03:08 +0100

    More fixes

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,12 +2,12 @@ import constants from '../utils/constants.js'
 
 const handlers = {
   get: (request, h) => {
-    if (process.env.REGISTER_START_ROUTES === 'true') {
-      request.yar.reset()
-      return h.view(constants.views.HOME)
-    } else {
-      return h.redirect(constants.urls.GOV_UK_SERVICE_HOME).permanent()
-    }
+    // if (process.env.REGISTER_START_ROUTES === 'true') {
+    request.yar.reset()
+    return h.view(constants.views.HOME)
+    // } else {
+    //   return h.redirect(constants.urls.GOV_UK_SERVICE_HOME).permanent()
+    // }
   }
 }
 
