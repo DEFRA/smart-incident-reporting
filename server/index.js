51f95f2d1038e302aa7935f106ea967b100bf79a
RyonLeightonDEFRA <ryon.leighton@defra.gov.uk>
2026-07-08 14:31:45 +0100

    update expiration config

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import logging from './plugins/logging.js'
 import session from './plugins/session.js'
 import onPostHandler from './plugins/on-post-handler.js'
 
-const expire = 168 * 60 * 60 * 1000
+const expire = 15 * 60 * 1000
 
 const createServer = async options => {
   // Create the hapi server
