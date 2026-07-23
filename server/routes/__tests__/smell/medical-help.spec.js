f077acb54ce18d10e0e5c784d8e7eaef48337ec8
noodles979 <ian.noonan@defra.gov.uk>
2026-07-23 12:31:12 +0100

    update to medical help title

--- server/routes/__tests__/smell/medical-help.spec.js
+++ server/routes/__tests__/smell/medical-help.spec.js
@@ -13,7 +13,7 @@ const baseAnswer = {
 describe(url, () => {
   describe('GET', () => {
     it(`Should return success response and correct view for ${url}`, async () => {
-      await submitGetRequest({ url }, question.text)
+      await submitGetRequest({ url }, 'Have you had to get any medical help or treatment?')
     })
   })
   describe('POST', () => {
