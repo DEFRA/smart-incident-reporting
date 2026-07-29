61099c0be7f2dbff801a43234f11f2f7f85aefac
Scott <scott.dormand@defra.gov.uk>
2026-07-29 10:12:55 +0100

    remove smell

--- server/routes/smell/report-local-council.js
+++
@@ -1,9 +0,0 @@
-import constants from '../../utils/constants.js'
-
-export default [
-  {
-    method: 'GET',
-    path: constants.routes.SMELL_REPORT_LOCAL_COUNCIL,
-    handler: (_request, h) => h.redirect(constants.routes.SMELL_SOURCE)
-  }
-]
--- server/routes/smell/source-details.js
+++
@@ -1,9 +0,0 @@
-import constants from '../../utils/constants.js'
-
-export default [
-  {
-    method: 'GET',
-    path: constants.routes.SMELL_SOURCE_DETAILS,
-    handler: (_request, h) => h.redirect(constants.routes.SMELL_SOURCE)
-  }
-]
