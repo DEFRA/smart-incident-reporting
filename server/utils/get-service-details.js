8bf378efc6c22c22a0c42f166b508eb686083c27
Scott <scott.dormand@defra.gov.uk>
2026-07-31 15:08:51 +0100

    sonar

--- server/utils/get-service-details.js
+++ server/utils/get-service-details.js
@@ -2,53 +2,26 @@ import constants from './constants.js'
 
 const generatePageTitle = serviceName => `${serviceName.charAt(0).toLowerCase()}${serviceName.slice(1)}`
 
+const routeServiceMap = [
+  { route: constants.routes.WATER_POLLUTION, name: constants.serviceNames.WATER_POLLUTION, url: constants.urls.GOV_UK_WATER_POLLUTION },
+  { route: constants.routes.SMELL, name: constants.serviceNames.SMELL, url: constants.urls.GOV_UK_SMELL },
+  { route: constants.routes.ILLEGAL_FISHING, name: constants.serviceNames.ILLEGAL_FISHING, url: constants.urls.GOV_UK_ILLEGAL_FISHING },
+  { route: constants.routes.BLOCKAGE, name: constants.serviceNames.BLOCKAGE, url: constants.urls.GOV_UK_BLOCKAGE },
+  { route: constants.routes.NOISE, name: constants.serviceNames.NOISE, url: constants.urls.GOV_UK_NOISE },
+  { route: constants.routes.LITTER, name: constants.serviceNames.LITTER, url: constants.urls.GOV_UK_LITTER },
+  { route: constants.routes.VERMIN, name: constants.serviceNames.VERMIN, url: constants.urls.GOV_UK_VERMIN },
+  { route: constants.routes.DUST, name: constants.serviceNames.DUST, url: constants.urls.GOV_UK_DUST },
+  { route: constants.routes.MUD, name: constants.serviceNames.MUD, url: constants.urls.GOV_UK_MUD }
+]
+
 const getServiceDetails = (filename = '') => {
   let serviceName = constants.serviceNames.DEFAULT
   let serviceUrl = constants.urls.GOV_UK_SERVICE_HOME
 
-  if (filename.includes(`${constants.routes.WATER_POLLUTION}/`)) {
-    serviceName = constants.serviceNames.WATER_POLLUTION
-    serviceUrl = constants.urls.GOV_UK_WATER_POLLUTION
-  }
-
-  if (filename.includes(`${constants.routes.SMELL}/`)) {
-    serviceName = constants.serviceNames.SMELL
-    serviceUrl = constants.urls.GOV_UK_SMELL
-  }
-
-  if (filename.includes(`${constants.routes.ILLEGAL_FISHING}/`)) {
-    serviceName = constants.serviceNames.ILLEGAL_FISHING
-    serviceUrl = constants.urls.GOV_UK_ILLEGAL_FISHING
-  }
-
-  if (filename.includes(`${constants.routes.BLOCKAGE}/`)) {
-    serviceName = constants.serviceNames.BLOCKAGE
-    serviceUrl = constants.urls.GOV_UK_BLOCKAGE
-  }
-
-  if (filename.includes(`${constants.routes.NOISE}/`)) {
-    serviceName = constants.serviceNames.NOISE
-    serviceUrl = constants.urls.GOV_UK_NOISE
-  }
-
-  if (filename.includes(`${constants.routes.LITTER}/`)) {
-    serviceName = constants.serviceNames.LITTER
-    serviceUrl = constants.urls.GOV_UK_LITTER
-  }
-
-  if (filename.includes(`${constants.routes.VERMIN}/`)) {
-    serviceName = constants.serviceNames.VERMIN
-    serviceUrl = constants.urls.GOV_UK_VERMIN
-  }
-
-  if (filename.includes(`${constants.routes.DUST}/`)) {
-    serviceName = constants.serviceNames.DUST
-    serviceUrl = constants.urls.GOV_UK_DUST
-  }
-
-  if (filename.includes(`${constants.routes.MUD}/`)) {
-    serviceName = constants.serviceNames.MUD
-    serviceUrl = constants.urls.GOV_UK_MUD
+  const match = routeServiceMap.find(entry => filename.includes(`${entry.route}/`))
+  if (match) {
+    serviceName = match.name
+    serviceUrl = match.url
   }
 
   if (process.env.REGISTER_START_ROUTES === 'true') {
