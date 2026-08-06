a0283137f0d18481b0fd04575d96f0b3b7384f76
Scott <scott.dormand@defra.gov.uk>
2026-08-06 14:59:52 +0100

    report local council page

--- server/routes/rars/report-local-council.js
+++ server/routes/rars/report-local-council.js
@@ -1,13 +1,22 @@
 import constants from '../../utils/constants.js'
-import { getServiceDetails } from '../../utils/helpers.js'
+import { getServiceDetails, titleHelper } from '../../utils/helpers.js'
+
+const question = 'Report the {problem} to your local council'
+const verminQuestion = 'Report the {vermin} to your local council'
 
 const createReportLocalCouncilRoutes = ({ problem, route }) => {
   const serviceDetails = getServiceDetails(problem)
 
   const handlers = {
-    get: async (_request, h) => {
+    get: async (request, h) => {
+      const { title, pageTitle } = titleHelper(request, question, verminQuestion, problem)
+      const issue = problem === 'vermin'
+        ? request.yar.get(constants.redisKeys.VERMIN_TYPE_SELECTED)
+        : problem
       return h.view(constants.views.RARS_REPORT_LOCAL_COUNCIL, {
-        problem,
+        title,
+        pageTitle,
+        issue,
         ...serviceDetails
       })
     }
