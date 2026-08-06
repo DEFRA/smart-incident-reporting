54205c7aa8d1e5145b6201c2d52f444ec6a1aae7
Scott <scott.dormand@defra.gov.uk>
2026-08-06 15:21:29 +0100

    Revert "report local council page"
    This reverts commit a0283137f0d18481b0fd04575d96f0b3b7384f76.

--- server/routes/__tests__/rars/report-local-council.spec.js
+++ server/routes/__tests__/rars/report-local-council.spec.js
@@ -1,6 +1,5 @@
 import { submitGetRequest } from '../../../__test-helpers__/server.js'
 import constants from '../../../utils/constants.js'
-import createReportLocalCouncilRoutes from '../../rars/report-local-council.js'
 
 const problems = [
   {
@@ -36,57 +35,11 @@ const problems = [
 ]
 
 describe('RARS Report Local Council Routes', () => {
-  describe.each(problems)('$problem report local council', ({ problem, url, header }) => {
+  describe.each(problems)('$problem report local council', ({ url, header }) => {
     describe('GET', () => {
       it('Should return success response and correct view', async () => {
-        const sessionData = problem === 'vermin'
-          ? { [constants.redisKeys.VERMIN_TYPE_SELECTED]: 'vermin' }
-          : {}
-
-        await submitGetRequest({ url }, header, constants.statusCodes.OK, sessionData)
+        await submitGetRequest({ url }, header)
       })
     })
-  })
-
-  const createRequest = selectedVermin => ({
-    yar: {
-      get: jest.fn(key => (key === constants.redisKeys.VERMIN_TYPE_SELECTED ? selectedVermin : undefined))
-    }
-  })
-
-  it.each([
-    { problem: 'smell', route: constants.routes.SMELL_REPORT_LOCAL_COUNCIL },
-    { problem: 'noise', route: constants.routes.NOISE_REPORT_LOCAL_COUNCIL },
-    { problem: 'dust', route: constants.routes.DUST_REPORT_LOCAL_COUNCIL },
-    { problem: 'litter', route: constants.routes.LITTER_REPORT_LOCAL_COUNCIL },
-    { problem: 'mud', route: constants.routes.MUD_REPORT_LOCAL_COUNCIL }
-  ])('passes title, pageTitle and issue for $problem', async ({ problem, route }) => {
-    const [localCouncilRoute] = createReportLocalCouncilRoutes({ problem, route })
-    const view = jest.fn()
-
-    await localCouncilRoute.handler(createRequest(), { view })
-
-    expect(view).toHaveBeenCalledWith(constants.views.RARS_REPORT_LOCAL_COUNCIL, expect.objectContaining({
-      title: `Report the ${problem} to your local council`,
-      pageTitle: `Report the ${problem} to your local council`,
-      issue: problem
-    }))
-  })
-
-  it('passes title, pageTitle and issue for vermin from session', async () => {
-    const selectedVermin = 'rats'
-    const [localCouncilRoute] = createReportLocalCouncilRoutes({
-      problem: 'vermin',
-      route: constants.routes.VERMIN_REPORT_LOCAL_COUNCIL
-    })
-    const view = jest.fn()
-
-    await localCouncilRoute.handler(createRequest(selectedVermin), { view })
-
-    expect(view).toHaveBeenCalledWith(constants.views.RARS_REPORT_LOCAL_COUNCIL, expect.objectContaining({
-      title: `Report the ${selectedVermin} to your local council`,
-      pageTitle: `Report the ${selectedVermin} to your local council`,
-      issue: selectedVermin
-    }))
   })
 })
--- server/routes/rars/report-local-council.js
+++ server/routes/rars/report-local-council.js
@@ -1,22 +1,13 @@
 import constants from '../../utils/constants.js'
-import { getServiceDetails, titleHelper } from '../../utils/helpers.js'
-
-const question = 'Report the {problem} to your local council'
-const verminQuestion = 'Report the {vermin} to your local council'
+import { getServiceDetails } from '../../utils/helpers.js'
 
 const createReportLocalCouncilRoutes = ({ problem, route }) => {
   const serviceDetails = getServiceDetails(problem)
 
   const handlers = {
-    get: async (request, h) => {
-      const { title, pageTitle } = titleHelper(request, question, verminQuestion, problem)
-      const issue = problem === 'vermin'
-        ? request.yar.get(constants.redisKeys.VERMIN_TYPE_SELECTED)
-        : problem
+    get: async (_request, h) => {
       return h.view(constants.views.RARS_REPORT_LOCAL_COUNCIL, {
-        title,
-        pageTitle,
-        issue,
+        problem,
         ...serviceDetails
       })
     }
