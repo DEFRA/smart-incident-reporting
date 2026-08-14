06d418e35ff4fe4f70b6094935e0ed29545aa320
sujithvg <sujithvg@users.noreply.github.com>
2026-08-14 10:13:33 +0100

    Technical implementation of SIR-2527 (#420)

--- server/routes/__tests__/smell/choose-address.spec.js
+++ server/routes/__tests__/smell/choose-address.spec.js
@@ -72,6 +72,107 @@ const incorrectPostcodeResponse = {
   }
 }
 
+const flatAddressApiResponse = {
+  payload: {
+    header: {
+      uri: 'https://api.os.uk/search/places/v1/postcode?postcode=TE1%200ST&lr=EN&fq=logical_status_code%3A1%20logical_status_code%3A6&dataset=DPA&offset=0&maxresults=100',
+      query: 'postcode=TE1 0ST',
+      offset: 0,
+      totalresults: 4,
+      format: 'JSON',
+      dataset: 'DPA',
+      lr: 'EN',
+      maxresults: 100,
+      epoch: '115',
+      lastupdate: '2025-03-12',
+      filter: 'fq=logical_status_code:1 logical_status_code:6',
+      output_srs: 'EPSG:27700'
+    },
+    results: [
+      {
+        DPA: {
+          UPRN: '10001150010',
+          ADDRESS: 'FLAT 10, TEST HOUSE, 5, EXAMPLE STREET, TESTTOWN, TE1 0ST',
+          POSTCODE: 'TE1 0ST',
+          X_COORDINATE: 543210.0,
+          Y_COORDINATE: 182345.0
+        }
+      },
+      {
+        DPA: {
+          UPRN: '10001150002',
+          ADDRESS: 'FLAT 2, TEST HOUSE, 5, EXAMPLE STREET, TESTTOWN, TE1 0ST',
+          POSTCODE: 'TE1 0ST',
+          X_COORDINATE: 543210.0,
+          Y_COORDINATE: 182345.0
+        }
+      },
+      {
+        DPA: {
+          UPRN: '10001150001',
+          ADDRESS: 'FLAT 1, TEST HOUSE, 5, EXAMPLE STREET, TESTTOWN, TE1 0ST',
+          POSTCODE: 'TE1 0ST',
+          X_COORDINATE: 543210.0,
+          Y_COORDINATE: 182345.0
+        }
+      },
+      {
+        DPA: {
+          UPRN: '10001150011',
+          ADDRESS: 'FLAT 11, TEST HOUSE, 5, EXAMPLE STREET, TESTTOWN, TE1 0ST',
+          POSTCODE: 'TE1 0ST',
+          X_COORDINATE: 543210.0,
+          Y_COORDINATE: 182345.0
+        }
+      }
+    ]
+  }
+}
+
+const mixedStreetAddressApiResponse = {
+  payload: {
+    header: {
+      uri: 'https://api.os.uk/search/places/v1/postcode?postcode=TE2%201LP&lr=EN&fq=logical_status_code%3A1%20logical_status_code%3A6&dataset=DPA&offset=0&maxresults=100',
+      query: 'postcode=TE2 1LP',
+      offset: 0,
+      totalresults: 23,
+      format: 'JSON',
+      dataset: 'DPA',
+      lr: 'EN',
+      maxresults: 100,
+      epoch: '115',
+      lastupdate: '2025-03-12',
+      filter: 'fq=logical_status_code:1 logical_status_code:6',
+      output_srs: 'EPSG:27700'
+    },
+    results: [
+      { DPA: { UPRN: '20001150001', ADDRESS: 'UNIT 1, 12, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150002', ADDRESS: 'N G J MANAGEMENT LTD, UNIT 3, 12, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150003', ADDRESS: 'LAKE LAW LLP, 12, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150004', ADDRESS: 'STONE MILL, 14, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150005', ADDRESS: 'FLAT 1, 30, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150006', ADDRESS: 'FLAT 2, 30, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150007', ADDRESS: 'FLAT 3, 30, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150008', ADDRESS: 'HUNGRY HIPPO, 38, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150009', ADDRESS: 'CLARK GROUP, GRANGE HOUSE, 42, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150010', ADDRESS: '1 MEADOW COURT, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150011', ADDRESS: 'C P L TESTVILLE LTD, 2 MEADOW COURT, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150012', ADDRESS: '26-28, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150013', ADDRESS: 'LONG LANE HOTEL, 29-31, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150014', ADDRESS: 'EDGE Q S LTD, 3 MEADOW COURT, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150015', ADDRESS: 'ROBERT MOORE DAVIES SOLICITORS, 32-36, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150016', ADDRESS: '34A, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150017', ADDRESS: '4-8 MEADOW COURT, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150018', ADDRESS: 'STRATEGY ADVISORS, THE OLD SCHOOL HOUSE, MEADOW COURT, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150019', ADDRESS: 'BESPOKE CARE AND SUPPORT LTD, CROWN HOUSE, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150020', ADDRESS: 'INTELLIGENT FINANCE LTD, CROWN HOUSE, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150021', ADDRESS: 'NATIONAL ARTS MUSEUM, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150022', ADDRESS: 'PITCHER AND LUTE, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } },
+      { DPA: { UPRN: '20001150023', ADDRESS: 'ST. JOHNS IN THE LONG LANE, LONG LANE, TESTVILLE, TE2 1LP', POSTCODE: 'TE2 1LP', X_COORDINATE: 454321.0, Y_COORDINATE: 337654.0 } }
+    ]
+  }
+}
+
 const postSessionData1 = {
   'smell/choose-address': {
     resultsFound: true,
@@ -372,6 +473,75 @@ describe(url, () => {
       expect(response.payload).toContain('<input class="govuk-radios__input" id="answerId-3" name="answerId" type="radio" value="10001142727" checked>')
       expect(response.payload).toContain('<a href="/smell/find-address" class="govuk-link">Change search</a>')
       expect(response.payload).toContain('<a href="/smell/location-address" class="govuk-link">Enter address manually</a>')
+    })
+    it(`Happy: Should order flat numbers numerically for matching addresses ${url}`, async () => {
+      const sessionData = {
+        'smell/find-address': {
+          buildingDetails: 'Test House',
+          postcode: 'TE1 0ST'
+        }
+      }
+
+      findByPostcode.mockResolvedValueOnce(flatAddressApiResponse)
+      const response = await submitGetRequest({ url }, header, constants.statusCodes.OK, sessionData)
+
+      const flat1Index = response.payload.indexOf('Flat 1, Test House, 5, Example Street, Testtown, TE1 0ST')
+      const flat2Index = response.payload.indexOf('Flat 2, Test House, 5, Example Street, Testtown, TE1 0ST')
+      const flat10Index = response.payload.indexOf('Flat 10, Test House, 5, Example Street, Testtown, TE1 0ST')
+      const flat11Index = response.payload.indexOf('Flat 11, Test House, 5, Example Street, Testtown, TE1 0ST')
+
+      expect(flat1Index).toBeGreaterThan(-1)
+      expect(flat2Index).toBeGreaterThan(-1)
+      expect(flat10Index).toBeGreaterThan(-1)
+      expect(flat11Index).toBeGreaterThan(-1)
+      expect(flat1Index).toBeLessThan(flat2Index)
+      expect(flat2Index).toBeLessThan(flat10Index)
+      expect(flat10Index).toBeLessThan(flat11Index)
+    })
+    it(`Happy: Should sort a mixed street of named and numbered addresses alphabetically with numeric ordering ${url}`, async () => {
+      const sessionData = {
+        'smell/find-address': {
+          buildingDetails: '',
+          postcode: 'TE2 1LP'
+        }
+      }
+
+      findByPostcode.mockResolvedValueOnce(mixedStreetAddressApiResponse)
+      const response = await submitGetRequest({ url }, header, constants.statusCodes.OK, sessionData)
+
+      const addressOrder = [
+        '1 Meadow Court, Long Lane, Testville, TE2 1LP',
+        '4-8 Meadow Court, Long Lane, Testville, TE2 1LP',
+        '26-28, Long Lane, Testville, TE2 1LP',
+        '34a, Long Lane, Testville, TE2 1LP',
+        'Bespoke Care And Support Ltd, Crown House, Long Lane, Testville, TE2 1LP',
+        'C P L Testville Ltd, 2 Meadow Court, Long Lane, Testville, TE2 1LP',
+        'Clark Group, Grange House, 42, Long Lane, Testville, TE2 1LP',
+        'Edge Q S Ltd, 3 Meadow Court, Long Lane, Testville, TE2 1LP',
+        'Flat 1, 30, Long Lane, Testville, TE2 1LP',
+        'Flat 2, 30, Long Lane, Testville, TE2 1LP',
+        'Flat 3, 30, Long Lane, Testville, TE2 1LP',
+        'Hungry Hippo, 38, Long Lane, Testville, TE2 1LP',
+        'Intelligent Finance Ltd, Crown House, Long Lane, Testville, TE2 1LP',
+        'Lake Law Llp, 12, Long Lane, Testville, TE2 1LP',
+        'Long Lane Hotel, 29-31, Long Lane, Testville, TE2 1LP',
+        'N G J Management Ltd, Unit 3, 12, Long Lane, Testville, TE2 1LP',
+        'National Arts Museum, Long Lane, Testville, TE2 1LP',
+        'Pitcher And Lute, Long Lane, Testville, TE2 1LP',
+        'Robert Moore Davies Solicitors, 32-36, Long Lane, Testville, TE2 1LP',
+        'St. Johns In The Long Lane, Long Lane, Testville, TE2 1LP',
+        'Stone Mill, 14, Long Lane, Testville, TE2 1LP',
+        'Strategy Advisors, The Old School House, Meadow Court, Long Lane, Testville, TE2 1LP',
+        'Unit 1, 12, Long Lane, Testville, TE2 1LP'
+      ]
+
+      const indices = addressOrder.map(addr => response.payload.indexOf(addr))
+
+      indices.forEach(index => expect(index).toBeGreaterThan(-1))
+
+      for (let i = 0; i < indices.length - 1; i++) {
+        expect(indices[i]).toBeLessThan(indices[i + 1])
+      }
     })
     it(`Sad: Should return No address found message for incorrect postcode ${url}`, async () => {
       const sessionData = {
