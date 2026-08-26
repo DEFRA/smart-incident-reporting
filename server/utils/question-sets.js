559fa5ffd8f66cc262ccca414e71d7fe0d7a92df
Scott Dormand <61588381+ScottDormand96@users.noreply.github.com>
2026-08-26 10:02:47 +0100

    Revert "Did smell cause health problems (#428)"
    This reverts commit affdd5c2cc46a18413e50dadf0b739c3d3a49474.

--- server/utils/question-sets.js
+++ server/utils/question-sets.js
@@ -717,10 +717,6 @@ const questionSets = {
           mentalHealthIssues: {
             answerId: 2508,
             text: 'Mental health issues, for example stress'
-          },
-          disturbedSleep: {
-            answerId: 2509,
-            text: 'Disturbed sleep'
           }
         }
       },
