714eb77da286f1204bb25637c022614b3d893afd
Scott <scott.dormand@defra.gov.uk>
2026-08-28 12:47:14 +0100

    code smell

--- server/routes/smell/description.js
+++ server/routes/smell/description.js
@@ -48,7 +48,7 @@ const buildAnswers = (answerId, somethingElseDetails) => {
     })
   })
 
-  if (answerId.indexOf(question.answers.somethingElse.answerId.toString()) > -1 && somethingElseDetails) {
+  if (answerId.includes(question.answers.somethingElse.answerId.toString()) && somethingElseDetails) {
     answers.push({
       ...baseAnswer,
       answerId: question.answers.somethingElseDetails.answerId,
