c4e470f7aaeddf367b5cac2b201f3f508712ba5e
Scott <scott.dormand@defra.gov.uk>
2026-09-16 07:30:54 +0100

    update max length

--- server/utils/__tests__/validation.spec.js
+++ server/utils/__tests__/validation.spec.js
@@ -52,6 +52,14 @@ describe('validation', () => {
 
       expect(maxLength(valueWithinLimit, characterLimit)).toBeFalsy()
     })
+
+    it('should count CRLF line breaks as a single character, matching the browser character count', () => {
+      // a textarea submits "\r\n" for each line break, but the browser's character count treats it as "\n"
+      const valueWithLineBreaks = 'a'.repeat(9) + '\r\n' + 'a'.repeat(9)
+      const characterLimit = 19
+
+      expect(maxLength(valueWithLineBreaks, characterLimit)).toBeFalsy()
+    })
   })
 
   describe('postcode', () => {
