c7c21bedd5ae8e7e5687edf66c70a352e1703350
sujithvg <sujithvg@users.noreply.github.com>
2026-09-22 11:22:03 +0100

    Added character count component to other journeys

--- server/routes/__tests__/water-pollution/location-description.spec.js
+++ server/routes/__tests__/water-pollution/location-description.spec.js
@@ -73,5 +73,29 @@ describe(url, () => {
       expect(response.payload).toContain('There is a problem')
       expect(response.payload).toContain('Enter a description of where you&#39;ve seen pollution')
     })
+    it('Sad: errors when locationDescription exceeds the character limit', async () => {
+      const locationDescription = 'a'.repeat(constants.locationDescriptionCharacterLimit + 1)
+      const options = {
+        url,
+        payload: {
+          locationDescription
+        }
+      }
+      const response = await submitPostRequest(options, constants.statusCodes.OK)
+      expect(response.payload).toContain('There is a problem')
+      expect(response.payload).toContain(`Location description must be ${constants.locationDescriptionCharacterLimit} characters or less`)
+      expect(response.request.yar.get(constants.redisKeys.WATER_POLLUTION_LOCATION_DESCRIPTION)).toBeFalsy()
+    })
+    it('Happy: accepts a locationDescription at the character limit', async () => {
+      const locationDescription = 'a'.repeat(constants.locationDescriptionCharacterLimit)
+      const options = {
+        url,
+        payload: {
+          locationDescription
+        }
+      }
+      const response = await submitPostRequest(options)
+      expect(response.headers.location).toEqual(constants.routes.WATER_POLLUTION_WHEN)
+    })
   })
 })
--- server/routes/illegal-fishing/location-description.js
+++ server/routes/illegal-fishing/location-description.js
@@ -1,6 +1,7 @@
 import constants from '../../utils/constants.js'
 import { getErrorSummary } from '../../utils/helpers.js'
 import { questionSets } from '../../utils/question-sets.js'
+import { maxLength } from '../../utils/validation.js'
 
 const question = questionSets.ILLEGAL_FISHING.questions.ILLEGAL_FISHING_LOCATION_DESCRIPTION
 
@@ -48,6 +49,13 @@ const validatePayload = locationDescription => {
       text: 'Enter a description of where you\'ve seen illegal fishing',
       href: '#locationDescription'
     })
+  } else if (maxLength(locationDescription, constants.locationDescriptionCharacterLimit)) {
+    errorSummary.errorList.push({
+      text: `Location description must be ${constants.locationDescriptionCharacterLimit} characters or less`,
+      href: '#locationDescription'
+    })
+  } else {
+    // do nothing
   }
   return errorSummary
 }
